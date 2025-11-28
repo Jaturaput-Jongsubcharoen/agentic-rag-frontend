79656ee24f04a50be6687bece0e76654b391a6ed
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2025-11-28 15:47:04 -0500

    required import React from "react";

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import RagChat from "./components/RagChat";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return <RagChat/>;
+  return <RagChat />;
 }
 
 export default App;
